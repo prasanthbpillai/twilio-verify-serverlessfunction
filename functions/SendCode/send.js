a7e43210b05e7e3410c787265e2adf8964851cde
Prasanth Pillai <prasanthbpillai@gmail.com>
2020-02-24 13:07:42 +1100

    Update send.js

--- functions/SendCode/send.js
+++ functions/SendCode/send.js
@@ -1,7 +1,6 @@
 exports.handler = function(context, event, callback) {
 	
-  const accountSid = 'ACce2d4"b78d2e2f025a2fae3d6c78577';
-  const authToken = 'd29f1853d31c7ff794e1c7e74611cc53';
+
   const client = context.getTwilioClient();
   console.log(event.to);	
   console.log("VerifyService " + context.DEFAULT_VERIFY_SERVICE);
